1d8f2df0d79e81bfb779e3e8555736a7a6d5e54b
blackshadowzz <rymang66@gmail.com>
2023-08-18 22:59:01 +0700

    updated

---
+++ SRMS.API/SRMS.API/Migrations/20230818155327_02.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SRMS.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class _02 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Levels",
+                columns: table => new
+                {
+                    LevelId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LevelName = table.Column<string>(type: "varchar(30)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Levels", x => x.LevelId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registrations",
+                columns: table => new
+                {
+                    RegistrationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    NameInKhmer = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Sex = table.Column<string>(type: "varchar(20)", nullable: true),
+                    DateofBirth = table.Column<DateTime>(type: "Date", nullable: true),
+                    PhoneNo = table.Column<string>(type: "varchar(20)", nullable: true),
+                    Telegram = table.Column<string>(type: "varchar(50)", nullable: true),
+                    RegistrationDated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registrations", x => x.RegistrationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    SubjectId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubjectName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Duration = table.Column<string>(type: "varchar(50)", nullable: true),
+                    SubjectCode = table.Column<string>(type: "varchar(15)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(200)", nullable: true),
+                    TypeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.SubjectId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubjectTypes",
+                columns: table => new
+                {
+                    TypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubjectTypes", x => x.TypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegistrationLines",
+                columns: table => new
+                {
+                    RegistrationLineId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RegistrationId = table.Column<int>(type: "int", nullable: true),
+                    SubjectId = table.Column<int>(type: "int", nullable: true),
+                    LevelId = table.Column<int>(type: "int", nullable: true),
+                    Durations = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartHour = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EndHour = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    PricPer = table.Column<decimal>(type: "decimal(18,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegistrationLines", x => x.RegistrationLineId);
+                    table.ForeignKey(
+                        name: "FK_RegistrationLines_Registrations_RegistrationId",
+                        column: x => x.RegistrationId,
+                        principalTable: "Registrations",
+                        principalColumn: "RegistrationId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RegistrationLines_RegistrationId",
+                table: "RegistrationLines",
+                column: "RegistrationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Levels");
+
+            migrationBuilder.DropTable(
+                name: "RegistrationLines");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+
+            migrationBuilder.DropTable(
+                name: "SubjectTypes");
+
+            migrationBuilder.DropTable(
+                name: "Registrations");
+        }
+    }
+}
